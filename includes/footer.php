79adc54376f6f2188dfa37bcc08ab144ae6772a6
ThijsVanLoo1 <thijs30042002@gmail.com>
2025-06-06 14:06:42 +0200

    Versie 1.0.0 fix

--- includes/footer.php
+++
@@ -1,9 +0,0 @@
-<?php
-echo '<nav>
-            <h2>&copy; Studio Scale</h2>
-            <div class="flex-row">
-                <a href="#"><i class="fa-brands fa-tiktok"></i></a>
-                <a href="#"><i class="fa-brands fa-youtube"></i></a>
-                <a href="#"><i class="fa-brands fa-twitter"></i></a>
-            </div>
-        </nav>';
